371344cc8694e4c86f392321f665411e933dd220
Xavier Geerinck <thebillkidy@gmail.com>
2015-08-11 21:37:22 +0200

    Remove not deleted files

--- frontend/src/core/Dispatcher.js
+++
@@ -1,5 +0,0 @@
-/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
-
-import { Dispatcher } from 'flux';
-
-export default new Dispatcher();
--- frontend/src/js/index.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import App from './components/App/App';
-
-React.render(<App />, document.body);
