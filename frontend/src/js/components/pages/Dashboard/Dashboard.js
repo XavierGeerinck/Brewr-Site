1bb2364433096aa9cf0ac1cd07641a7cacd12c95
Xavier Geerinck <thebillkidy@gmail.com>
2015-10-06 13:04:01 +0200

    Add quick overview of the projects on the dashboard

--- frontend/src/js/components/pages/Dashboard/Dashboard.js
+++ frontend/src/js/components/pages/Dashboard/Dashboard.js
@@ -1,16 +1,53 @@
 import './Dashboard.css';
 import React, { PropTypes } from 'react';
 import DashboardLayout from '../../layouts/DashboardLayout';
+import AuthStore from '../../../stores/AuthStore';
 
+/**
+ * The dashboard gives a quick overview of the projects that belong to a organisation
+ * The detailed information about projects can be found under the projects tab.
+ */
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = this._getState();
     }
 
+    _getState() {
+        return {
+            user: AuthStore.user,
+            organisations: AuthStore.organisations || [],
+            selected_organisation: AuthStore.selected_organisation
+        }
+    }
+
+    // Listen on store changes
+    componentDidMount() {
+        this.changeListener = this._onChange.bind(this);
+        AuthStore.addChangeListener(this.changeListener);
+    }
+
+    componentWillUnmount() {
+        AuthStore.removeChangeListener(this.changeListener);
+    }
+
+    _onChange() {
+        this.setState(this._getState());
+    }
+
+    // Render
     render() {
+        const { user, organisations, selected_organisation } = this.state;
+        const projects = selected_organisation ? selected_organisation.projects : [];
+
         return (
             <DashboardLayout>
-                HELLO WORLD
+                {
+                    projects.map(p => {
+                        return <div>{p.name}<br />{p.description}</div>
+                    })
+                }
             </DashboardLayout>
         );
     }
