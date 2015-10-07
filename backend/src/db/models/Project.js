393cc78a048f81c11d6261b6635bb82f8d727f0f
Xavier Geerinck <thebillkidy@gmail.com>
2015-10-07 08:47:58 +0200

    Added GET project with the team members

--- backend/src/db/models/Project.js
+++ backend/src/db/models/Project.js
@@ -6,14 +6,22 @@ var Organisation = require('./Organisation');
 var Project = Bookshelf.model('Project', {
     tableName: 'project',
     hasTimestamps: true, // Define that we update the created_at and updated_at on change
+    hidden: [ 'users' ],
+    virtuals: {
+        members: function () {
+            var members = this.related('users');
+            members.push(this.related('created_by'));
+            return members;
+        }
+    },
     owner: function () {
         return this.belongsTo('User');
     },
     created_by: function() {
-        return this.belongsTo('User');
+        return this.belongsTo('User', 'created_by');
     },
     users: function () {
-        return this.belongsToMany('User', 'project_user', 'user_id');
+        return this.belongsToMany('User', 'project_user', 'project_id', 'user_id').withPivot('is_manager');
     },
     revision: function () {
         return this.belongsToMany('ProjectRevision');
--- backend/test/integration/controllers/project.js
+++ backend/test/integration/controllers/project.js
@@ -24,73 +24,114 @@ lab.experiment('[Controller] User', function() {
         return dbUtil.truncate().then(function() { done(); });
     });
 
-	it('[POST] /project/:id/members/:member_id should assign the member to a project of the organisation', function (done) {
+	// =====================
+	// ADD USERS TO PROJECT
+	// =====================
+	// it('[POST] /project/:id/members/:member_id should require that the member_id is a member of the organisation', function (done) {
+	//
+	// });
+	//
+	// it('[POST] /project/:id/members/:member_id should assign the member to a project of the organisation', function (done) {
+	//
+	// });
+	//
+	// it('[POST] /project/:id/members/:member_id should be able to add managers through a payload parameter', function (done) {
+	//
+	// });
+	//
+	// it('[POST] /project/:id/members/:member_id should only allow belongs-to-organisation-<orgid>-creator or manager to add managers to a project', function (done) {
+	//
+	// });
+	//
+	// it('[POST] /project/:id/members/:member_id requires belongs-to-organisation-<orgid>-project-<projectid>-manager or creator scope', function (done) {
+	//
+	// });
+	//
+	// it('[DELETE] /project/:id/members/:member_id should remove the member of a project of an organisation', function (done) {
+	//
+	// });
+	//
+	// it('[DELETE] /project/:id/members/:member_id requires belongs-to-organisation-<orgid>-project-<projectid>-manager or creator scope', function (done) {
+	//
+	// });
 
-	});
-
-	it('[POST] /project/:id/members/:member_id should be able to add managers through a payload parameter', function (done) {
-
-	});
-
-	it('[POST] /project/:id/members/:member_id should only allow belongs-to-organisation-<orgid>-creator or manager to add managers to a project', function (done) {
-
-	});
-
-	it('[POST] /project/:id/members/:member_id requires belongs-to-organisation-<orgid>-project-<projectid>-manager or creator scope', function (done) {
-
-	});
-
-	it('[DELETE] /project/:id/members/:member_id should remove the member of a project of an organisation', function (done) {
-
-	});
-
-	it('[DELETE] /project/:id/members/:member_id requires belongs-to-organisation-<orgid>-project-<projectid>-manager or creator scope', function (done) {
-
-	});
-
-	it('[GET] /project/:id should return an error if I do not have sufficient rights for it', function (done) {
-
-	});
-
-	it('[GET] /project/:id should return detailed information about the project such as team members', function (done) {
-
-	});
-
-	it('[DELETE] /project/:id can only be executed with the correct rights', function (done) {
-
-	});
-
-	it('[DELETE] /project/:id without a correct id should return an error', function (done) {
-
-	});
-
-	it('[DELETE] /project/:id should require the password of the logged in user', function (done) {
-
-	});
-
-    it('[DELETE] /project/:id should return success on deletion of the project', function (done) {
-        var request = {
-            method: 'POST',
-            url: '/auth/signup',
-            payload: {
-                email: '1@1.be',
-                password: 'somepassword',
-                firstName: 'John',
-                lastName: 'Doe'
+	// ==============
+	// PROJECT CRUD
+	// ==============
+	it('[GET] /organisation/:org_id/project/:id should return an error if I do not have sufficient rights for it', function (done) {
+		var request = {
+			method: 'GET',
+			url: '/organisation/3/project/7',
+            headers: {
+                Authorization: 'Bearer ' + fixtures['user_session'][0].token
             }
-        };
+		};
 
-        server.inject(request, function (res) {
-            expect(res.payload).to.exist();
-            expect(JSON.parse(res.payload).email).to.equal(request.payload.email);
+		server.inject(request, function (res) {
+			expect(res.payload).to.exist();
 
-            // Make sure the pass gets hashed!
-            expect(JSON.parse(res.payload).password).to.not.equal(request.payload.password);
+			expect(JSON.parse(res.payload).message).to.equal('Insufficient scope, expected any of: belongs-to-organisation-3-project-7-user');
 
-            expect(JSON.parse(res.payload).first_name).to.equal(request.payload.firstName);
-            expect(JSON.parse(res.payload).last_name).to.equal(request.payload.lastName);
+			done();
+		});
+	});
 
-            done();
-        });
-    });
+	it('[GET] /organisation/:org_id/project/:id should return detailed information about the project such as team members', function (done) {
+		var request = {
+			method: 'GET',
+			url: '/organisation/2/project/3',
+			headers: {
+				Authorization: 'Bearer ' + fixtures['user_session'][0].token
+			}
+		};
+
+		server.inject(request, function (res) {
+			expect(res.payload).to.exist();
+
+			expect(JSON.parse(res.payload).members).to.exist();
+			expect(JSON.parse(res.payload).members[0].email).to.equal(fixtures['user'][0].email);
+			expect(JSON.parse(res.payload).members[0].first_name).to.equal(fixtures['user'][0].first_name);
+			expect(JSON.parse(res.payload).members[0].last_name).to.equal(fixtures['user'][0].last_name);
+			expect(JSON.parse(res.payload).members.length).to.equal(2);
+			done();
+		});
+	});
+	//
+	// it('[DELETE] /organisation/:org_id/project/:id can only be executed with the correct rights', function (done) {
+	//
+	// });
+	//
+	// it('[DELETE] /organisation/:org_id/project/:id without a correct id should return an error', function (done) {
+	//
+	// });
+	//
+	// it('[DELETE] /organisation/:org_id/project/:id should require the password of the logged in user', function (done) {
+	//
+	// });
+
+    // it('[DELETE] /project/:id should return success on deletion of the project', function (done) {
+    //     var request = {
+    //         method: 'POST',
+    //         url: '/auth/signup',
+    //         payload: {
+    //             email: '1@1.be',
+    //             password: 'somepassword',
+    //             firstName: 'John',
+    //             lastName: 'Doe'
+    //         }
+    //     };
+	//
+    //     server.inject(request, function (res) {
+    //         expect(res.payload).to.exist();
+    //         expect(JSON.parse(res.payload).email).to.equal(request.payload.email);
+	//
+    //         // Make sure the pass gets hashed!
+    //         expect(JSON.parse(res.payload).password).to.not.equal(request.payload.password);
+	//
+    //         expect(JSON.parse(res.payload).first_name).to.equal(request.payload.firstName);
+    //         expect(JSON.parse(res.payload).last_name).to.equal(request.payload.lastName);
+	//
+    //         done();
+    //     });
+    // });
 });
