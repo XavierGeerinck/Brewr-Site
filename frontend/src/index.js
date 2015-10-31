ef141a0929659496dc583cd77083e195e9f8dd46
Xavier Geerinck <thebillkidy@gmail.com>
2015-10-31 16:28:47 +0100

    Refactoring

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import routes from './routes';
+
+// Install the routes
+ReactDOM.render(routes, document.getElementById('root'));
--- frontend/src/js/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import routes from './routes';
-
-import { render } from 'react-dom';
-// Install the routes
-render(routes, document.body);
